838cd953da7464fc7c8ee9f8cd92644193961d33
Jonathan Diego Rodriguez Rdz <jon_eldiablo@hotmail.com>
2022-05-23 17:10:12 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-node --experimental-json-modules update-version.js
+node update-version.js
+rm -rf ./lib/js/* && mkdir -p lib/js
+rm -rf ./lib/css/* && mkdir -p lib/css
+rm -rf ./lib/scss/* && mkdir -p lib/scss
+cp -r src/scss lib
 # construir desde "arriba" para no instalar las dependencias aquí
 # hay pedos si se está trabajando con la librería linkada (yarn link)
 cd ..
-yarn build
+yarn babel
 cd ./dbl-components
 #-------
 git add .
