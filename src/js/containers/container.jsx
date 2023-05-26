7105acfc42f101a8fc33085e19ce19d692b181f4
Jonathan Diego Rodriguez Rdz <jon_eldiablo@hotmail.com>
2023-05-25 18:29:22 -0600

    landscape

--- src/js/containers/container.jsx
+++ src/js/containers/container.jsx
@@ -25,29 +25,37 @@ export default class Container extends Component {
   breakpoint = false;
   waitBreakpoint = <Icons icon="spinner" classes="spinner" />;
 
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+  }
+
   updateSize() {
     const { fluid, fullWidth } = this.props;
     const containerType = (!fullWidth ? (fluid ? 'container-fluid' : 'container') : '');
     this.setState({ localClasses: [containerType, this.breakpoint, 'animate'].join(' ') });
   }
 
-  onResize = () => {
+  onResize() {
     clearTimeout(this.onResizeTimeout);
     this.onResizeTimeout = setTimeout(() => {
       if (!this.ref.current) return;
       let { offsetWidth: width, offsetHeight: height } = this.ref.current;
-      if (typeof this.props.onResize === 'function') {
-        this.props.onResize({ width, height });
-      }
       // TODO: no se toma en cuenta el ordenamiento de los breakpoints, ordenarlos
       //       y buscar la manera de empatar automagicamente con sass $container-max-widths
       this.breakpoint = Object.keys(this.props.breakpoints)
         .filter(br => width >= this.props.breakpoints[br])
         .pop();
-      eventHandler.dispatch('resize.' + this.props.name, {
+      this.orientation = width >= height ? 'landscape' : 'portrait';
+      const resp = {
         width, height,
-        breakpoint: this.breakpoint
-      });
+        breakpoint: this.breakpoint,
+        orientation: this.orientation
+      };
+      if (typeof this.props.onResize === 'function') {
+        this.props.onResize(resp);
+      }
+      eventHandler.dispatch('resize.' + this.props.name, resp);
       this.updateSize();
     }, 200);
   }
