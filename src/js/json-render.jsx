9e2428a66ec596de075b61be6c124e37dde7bcdf
Jonathan Diego Rodriguez Rdz <jon_eldiablo@hotmail.com>
2023-05-29 11:01:57 -0600

    table vertical

--- src/js/json-render.jsx
+++ src/js/json-render.jsx
@@ -5,8 +5,17 @@ import parseReact, { domToReact, attributesToProps } from "html-react-parser";
 import Icons from "./media/icons";
 import COMPONENTS from "./components";
 
+/**
+ * Clase utilizada para generar contenido dinámico en React a partir de una estructura de datos JSON.
+ *
+ * @class JsonRender
+ */
 export default class JsonRender {
 
+  /**
+   * Opciones para el análisis del contenido HTML.
+   * @type {Object}
+   */
   parseOpts = {
     replace: domNode => {
       let C7tReplace;
@@ -43,6 +52,11 @@ export default class JsonRender {
     }
   }
 
+  /**
+   * Crea una instancia de JsonRender.
+   * @param {Object} props - Las propiedades del componente.
+   * @param {Object} mutations - Las mutaciones para las secciones.
+   */
   constructor(props, mutations) {
     this.props = props;
     this.mutations = mutations;
@@ -50,6 +64,12 @@ export default class JsonRender {
     this.buildContent = this.buildContent.bind(this);
   }
 
+  /**
+   * Construye el contenido basado en la estructura de datos proporcionada.
+   * @param {any} content - El contenido a construir.
+   * @param {number} index - El índice del contenido.
+   * @returns {React.Component|React.Fragment|boolean} - El componente construido.
+   */
   buildContent(content, index) {
     if (!content) return false;
     if (typeof content === 'number') {
@@ -71,6 +91,12 @@ export default class JsonRender {
     return this.sections(content, index);
   }
 
+  /**
+   * Construye una sección basada en la información proporcionada.
+   * @param {Object} sectionRaw - Los datos de la sección.
+   * @param {number} i - El índice de la sección.
+   * @returns {React.Component|boolean} - El componente de la sección construido.
+   */
   sections(sectionRaw, i) {
     if (typeof this.mutations === 'function') {
       const m = this.mutations(sectionRaw.name, sectionRaw) || {};
@@ -116,119 +142,3 @@ export default class JsonRender {
   }
 
 }
-
-
-/**
- * import React from "react";
-import { NavLink, Link } from "react-router-dom";
-import parseReact, { domToReact, attributesToProps } from "html-react-parser";
-
-import Icons from "./media/icons";
-import COMPONENTS from "./components";
-
-const e = React.createElement;
-
-export default class JsonRender {
-
-  parseOpts = {
-    replace: domNode => {
-      let C7tReplace;
-      switch (domNode.name) {
-        case 'navlink':
-          C7tReplace = NavLink;
-          break;
-        case 'a':
-          if (!domNode.attribs.to && domNode.attribs.href) return;
-          C7tReplace = Link;
-          break;
-        case 'icons':
-          C7tReplace = Icons;
-          domNode.attribs.inline = domNode.attribs.inline === 'false' ? false : true;
-          break;
-        case 'textarea':
-        case 'input':
-          domNode.defaultValue = domNode.value;
-          domNode.defaultChecked = domNode.checked;
-          delete domNode.value;
-          delete domNode.checked;
-        default:
-          return;
-      }
-      Object.keys(domNode).forEach(k => {
-        if (k.match(/^on[A-Z]/)) {
-          domNode[k] = this.props[k];
-        }
-      });
-      return e(C7tReplace, attributesToProps(domNode.attribs), domToReact(domNode.children, this.parseOpts));
-    }
-  }
-
-  constructor(props, mutations) {
-    this.props = props;
-    this.mutations = mutations;
-    this.sections = this.sections.bind(this);
-    this.buildContent = this.buildContent.bind(this);
-  }
-
-  buildContent(content, index) {
-    if (!content) return false;
-    if (typeof content === 'number') {
-      return content;
-    } else if (typeof content === 'string') {
-      return e(React.Fragment, { key: content.name || index }, parseReact(content, this.parseOpts));
-    } else if (React.isValidElement(content)) {
-      try {
-        content.key = content.name || index;
-      } catch (error) {
-      }
-      return content;
-    } else if (Array.isArray(content)) return content.map(this.buildContent);
-    else if (typeof content === 'object' && typeof content.name !== 'string')
-      return Object.keys(content)
-        .map((name, i) => this.buildContent({ name, ...content[name] }, i));
-    return this.sections(content, index);
-  }
-
-  sections(sectionRaw, i) {
-    if (typeof this.mutations === 'function') {
-      const m = this.mutations(sectionRaw.name, sectionRaw) || {};
-      Object.assign(sectionRaw, m);
-    }
-    if (typeof sectionRaw.active === 'boolean' && !sectionRaw.active) return false;
-    const { component: componentName, content, label, message, errorMessage, ...section } = sectionRaw;
-    const { location, match, childrenIn = this.childrenIn, history, children } = this.props;
-    const Component = COMPONENTS[componentName] || (COMPONENTS.Component);
-    const componentProps = {
-      ...section,
-      label: this.buildContent(label),
-      message: this.buildContent(message),
-      errorMessage: this.buildContent(errorMessage),
-      location,
-      match,
-      history
-    }
-    if (Component.dontBuildContent) componentProps.content = content;
-    if (!Component.dontBuildContent && content && (childrenIn === section.name)) {
-      componentProps.children = e(React.Fragment, null, this.buildContent(content), children);
-    } else if (!Component.dontBuildContent && content) {
-      componentProps.children = this.buildContent(content);
-    } else if (childrenIn === section.name) {
-      componentProps.children = children;
-    }
-
-    const cnSection = [componentProps.name + '-section'];
-    if (this.props.test) cnSection.push('test-section-wrapper');
-    const exclusionSec = ['NavLink', 'Image', 'Link', 'Icons', 'Action',
-      'DropdownButtonContainer', 'ModalButtonContainer', 'DropdownItem']
-      .includes(componentName);
-    const Wrapper = componentProps.wrapper === false ? false :
-      componentProps.wrapper || Component.wrapper || 'section';
-    return (!Wrapper || exclusionSec || componentProps.tag ?
-      e(Component, { key: componentProps.name || i, ...componentProps }) :
-      e(Wrapper, { key: componentProps.name || i, className: cnSection.join(' ') }, e(Component, componentProps))
-    );
-  }
-
-}
-
- */
--- src/js/tables/table.jsx
+++ src/js/tables/table.jsx
@@ -9,7 +9,38 @@ import Icons from "../media/icons";
 import JsonRender from "../json-render";
 import DropdownContainer from "../containers/dropdown-container";
 
+/**
+ * @typedef {Object} FormatOptions
+ * @property {string} [format] - Formato para fechas y horas.
+ * @property {boolean} [locale] - Si se debe considerar la zona horaria local.
+ * @property {string} [currency] - Código de moneda para el formato de moneda.
+ * @property {string} ['true'] - Representación en cadena de un valor booleano `true`.
+ * @property {string} ['false'] - Representación en cadena de un valor booleano `false`.
+ */
+
+/**
+ * Objeto con funciones de formateo para diferentes tipos de datos.
+ * 
+ * @namespace
+ * @property {function(any, Object, Object, Object, string): React.Component} component - Formatea los datos a un componente.
+ * @property {function(any, FormatOptions=): string} date - Formatea los datos a una fecha.
+ * @property {function(any, FormatOptions=): string} datetime - Formatea los datos a un datetime.
+ * @property {function(any, FormatOptions=): string} currency - Formatea los datos a una moneda.
+ * @property {function(any, FormatOptions=): string} number - Formatea los datos a un número.
+ * @property {function(any, Object): string} boolean - Formatea los datos a un booleano.
+ */
 export const FORMATS = {
+  /**
+   * Formatea los datos en crudo a un componente.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} props - Propiedades del componente.
+   * @param {Object} data - Datos de la celda.
+   * @param {Object} jsonRender - Instancia de JsonRender.
+   * @param {String} colName - Nombre de la columna.
+   * @returns {React.Component} El componente formateado.
+   */
   component: (raw, props, data, jsonRender, colName) => {
     if (props.type === 'boolean') {
       props.value = !!raw;
@@ -22,25 +53,87 @@ export const FORMATS = {
     props.columnName = colName;
     return jsonRender.buildContent(props);
   },
+  /**
+   * Formatea los datos en crudo a una fecha.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} options - Opciones de formato de la fecha.
+   * @returns {String} La fecha formateada.
+   */
   date: (raw, { format: f = 'DD/MM/YYYY', locale = false } = {}) =>
     raw ? locale ? moment(raw).format(f) : moment.utc(raw).format(f) : '',
+  /**
+   * Formatea los datos en crudo a un datetime.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} options - Opciones de formato del datetime.
+   * @returns {String} El datetime formateado.
+   */
   datetime: (raw, { format: f = 'DD/MM/YYYY HH:mm', locale = false } = {}) =>
     raw ? locale ? moment(raw).format(f) : moment.utc(raw).format(f) : '',
+  /**
+   * Formatea los datos en crudo a una moneda.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} options - Opciones de formato de la moneda.
+   * @returns {String} La moneda formateada.
+   */
   currency: (raw, { locale = 'en-US', currency = 'USD' } = {}) =>
     ['string', 'number'].includes(typeof raw) ? (new Intl.NumberFormat(locale, {
       style: 'currency',
       currency
     })).format(raw) : '',
+  /**
+   * Formatea los datos en crudo a un número.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} options - Opciones de formato del número.
+   * @returns {String} El número formateado.
+   */
   number: (raw, { locale = 'en-US' } = {}) =>
     ['string', 'number'].includes(typeof raw) ?
       (new Number(raw)).toLocaleString(locale) : '',
+  /**
+   * Formatea los datos en crudo a un booleano.
+   *
+   * @function
+   * @param {any} raw - Los datos en crudo que deben ser formateados.
+   * @param {Object} options - Opciones de formato del booleano.
+   * @returns {String} El booleano formateado.
+   */
   boolean: (raw, { 'true': True = 'Yes', 'false': False = 'Not' }) => (raw ? True : False)
 }
 
+/**
+ * Agrega nuevas plantillas de formato al objeto FORMATS.
+ *
+ * Esta función utiliza `Object.assign` para fusionar las plantillas proporcionadas en el objeto FORMATS existente.
+ * Esto significa que si una plantilla con el mismo nombre ya existe en FORMATS, la nueva plantilla la sobrescribirá.
+ *
+ * @function
+ * @param {Object} newTemplates - Un objeto que contiene plantillas de formato a agregar. Las claves son los nombres de las plantillas y los valores son las funciones de formato.
+ * @returns {void}
+ * @example
+ * 
+ * addFormatTemplates({
+ *   newFormat: (raw, options) => { ... },
+ *   anotherFormat: (raw, options) => { ... }
+ * });
+ */
 export const addFormatTemplates = (newTemplates = {}) => {
   Object.assign(FORMATS, newTemplates);
 }
 
+/**
+ * Componente de celda de encabezado para una tabla.
+ *
+ * @class HeaderCell
+ * @extends {React.Component}
+ */
 export class HeaderCell extends React.Component {
 
   static jsClass = 'HeaderColumn';
@@ -55,6 +148,10 @@ export class HeaderCell extends React.Component {
     this.events = [];
   }
 
+  /**
+   * Método de ciclo de vida de React que se llama cuando el componente se ha montado.
+   * Se suscribe a eventos relevantes para este componente.
+   */
   componentDidMount() {
     const { col } = this.props;
     if (col.filter) {
@@ -65,16 +162,30 @@ export class HeaderCell extends React.Component {
     }
   }
 
+  /**
+   * Método de ciclo de vida de React que se llama cuando el componente se actualiza.
+   * Restablece la dirección de la clasificación si la clasificación se ha eliminado.
+   * @param {Object} prevProps - Las propiedades anteriores del componente.
+   * @param {Object} prevState - El estado anterior del componente.
+   */
   componentDidUpdate(prevProps, prevState) {
     if (!this.props.sort && this.state.sortDir) this.setState({ sortDir: null });
   }
 
+  /**
+   * Método de ciclo de vida de React que se llama cuando el componente está a punto de desmontarse.
+   * Cancela la suscripción a todos los eventos a los que se suscribió en componentDidMount.
+   */
   componentWillUnmount() {
     for (const event of this.events) {
       eventHandler.unsubscribe(event[0]);
     }
   }
 
+  /**
+   * Manejador de eventos para cambios en el filtro.
+   * @param {Object} data - Los datos del evento.
+   */
   onChangeFilter = (data) => {
     const { filter } = this.props.col;
     const searchActive = !!(Array.isArray(data[filter.name]) ?
@@ -83,6 +194,10 @@ export class HeaderCell extends React.Component {
     eventHandler.dispatch(this.props.tableName, data);
   }
 
+  /**
+   * Realiza la acción de ordenar las celdas en el encabezado.
+   * @param {string} dir - La dirección en la que se va a realizar la ordenación.
+   */
   sort(dir) {
     const { onSort, col } = this.props;
     const { sortDir } = this.state;
@@ -92,6 +207,10 @@ export class HeaderCell extends React.Component {
     if (typeof onSort === 'function') onSort(newDir ? { [col.name]: newDir } : null);
   }
 
+  /**
+   * Renderiza el componente.
+   * @returns {React.Component} El componente renderizado.
+   */
   render() {
     const { col, classes, icons, orderable, filterPos } = this.props;
     const { sortDir, searchActive } = this.state;
@@ -106,7 +225,7 @@ export class HeaderCell extends React.Component {
     ];
     const cnSearch = ['cursor-pointer'];
     if (!searchActive) cnSearch.push('text-muted');
-    return <th className={"align-middle " + col.name}>
+    return <th className={"align-middle " + col.name} scope="col">
       <div className="d-flex align-items-center">
         <div className={cn.join(' ')} style={style}>
           <span>{col.label}</span>
@@ -138,8 +257,18 @@ export class HeaderCell extends React.Component {
       </div>
     </th>
   }
+
 }
 
+/**
+* Clase base para la tabla.
+*
+* Este componente es responsable de renderizar una tabla a partir de un conjunto de datos proporcionado.
+* También proporciona funcionalidad para ordenar, filtrar y manejar eventos.
+*
+* @class Table
+* @extends {Component}
+*/
 export default class Table extends Component {
 
   static jsClass = 'Table';
@@ -152,7 +281,8 @@ export default class Table extends Component {
       caretUp: 'caret-up',
       caretDown: 'caret-down',
       search: 'search'
-    }
+    },
+    vertical: false
   }
 
   constructor(props) {
@@ -160,6 +290,12 @@ export default class Table extends Component {
     this.jsonRender = new JsonRender(props, props.mutations);
   }
 
+  /**
+   * Subscribes to the events when the component is mounted.
+   *
+   * @method componentDidMount
+   * @memberof Table
+   */
   componentDidMount() {
     this.events = [];
     Object.entries(this.props.columns).forEach(([key, col]) => {
@@ -173,6 +309,12 @@ export default class Table extends Component {
     });
   }
 
+  /**
+   * Unsubscribes from the events when the component is unmounted.
+   *
+   * @method componentWillUnmount
+   * @memberof Table
+   */
   componentWillUnmount() {
     for (const [eventName] of this.events) {
       eventHandler.unsubscribe(eventName);
@@ -180,12 +322,26 @@ export default class Table extends Component {
   }
 
   // Events
+  /**
+   * Handles sorting event.
+   *
+   * @method onSort
+   * @param {Object} orderBy - An object representing the column to be sorted.
+   * @memberof Table
+   */
   onSort = (orderBy) => {
     const { onChange } = this.props;
     this.setState({ orderBy: orderBy && Object.keys(orderBy).pop() });
     if (typeof onChange === 'function') onChange({ orderBy });
   }
 
+  /**
+   * Handles cell events.
+   *
+   * @method onEventCell
+   * @param {Object} dataRaw - Raw data of the event.
+   * @memberof Table
+   */
   onEventCell = (dataRaw) => {
     const data = {};
     for (const event in dataRaw) {
@@ -195,8 +351,17 @@ export default class Table extends Component {
   }
   //------
 
+  /**
+   * Maps HeaderCell components for each column.
+   *
+   * @method mapHeaderCell
+   * @param {Array} args - The column properties.
+   * @param {number} i - The index of the column.
+   * @returns {React.Component} - A HeaderCell component.
+   * @memberof Table
+   */
   mapHeaderCell = ([key, col], i) => {
-    const { colClasses, icons, orderable, name } = this.props;
+    const { colClasses, icons, orderable, name, vertical, headerClasses } = this.props;
     const { orderBy } = this.state;
     col.name = col.name || key;
     col.label = this.jsonRender.buildContent(col.label);
@@ -209,37 +374,46 @@ export default class Table extends Component {
       sort: orderBy === col.name,
       tableName: name
     };
+    if (vertical && headerClasses) props.colClasses += ' ' + headerClasses;
     return <HeaderCell key={i + '-' + col.name} {...props} />
   }
 
-  mapRows = (row, i) => {
-    const { columns, name, mapRows: mapRowsFunc, rowContent } = this.props;
-    const rowId = (row.id === 0 || !!row.id) ? row.id : i;
-    const rowKey = (row.id === 0 || !!row.id) ? (i + '-' + rowId) : i;
-    const cnRow = ['row-' + this.props.name, 'row-' + rowId];
+  /**
+   * Provides properties for a row.
+   *
+   * @method rowProps
+   * @param {Object} rowOrColumn - The data of the row or column.
+   * @param {number} i - The index of the row or column.
+   * @returns {Object} - Properties for the row.
+   * @memberof Table
+   */
+  rowProps = (rowOrColumn, i) => {
+    const { name, mapRows: mapRowsFunc, vertical } = this.props;
+    const id = vertical ? rowOrColumn.name : rowOrColumn.id;
+    const rowKey = (!!id || id === 0) ? (i + '-' + id) : i;
+    const cnRow = ['row-' + this.props.name, 'row-' + ((!!id || id === 0) ? id : i)];
     const { classes: rowClasses, ...rowProps } =
-      (typeof mapRowsFunc === 'function' && mapRowsFunc(name, row, i)) || {};
+      (typeof mapRowsFunc === 'function' && mapRowsFunc(name, rowOrColumn, i)) || {};
     if (rowClasses) cnRow.push(rowClasses);
 
-    const _row = <tr key={rowKey} {...rowProps} className={cnRow.join(' ')}>
-      {Object.entries(columns).map(([key, col], j) => this.mapCells(row, col.name ? col : { name: key, ...col }, j))}
-    </tr>;
-    if (rowContent) {
-      const clone = JSON.parse(JSON.stringify(rowContent));
-      const _theRowContent = this.jsonRender.buildContent({ ...clone, row, name: `${clone.name}.${i}` });
-      return <>
-        {_row}
-        <tr>
-          <td colSpan="1000">
-            {_theRowContent}
-          </td>
-        </tr>
-      </>
-    } else return _row;
+    return {
+      key: rowKey,
+      ...rowProps,
+      className: cnRow.join(' ')
+    };
+  };
 
-  }
-
-  mapCells = (rowData, col, i) => {
+  /**
+   * Maps cell components for each cell in a row.
+   *
+   * @method mapCell
+   * @param {Object} rowData - The data of the row.
+   * @param {Object} col - The properties of the column.
+   * @param {number} i - The index of the cell.
+   * @returns {React.Component} - A cell component.
+   * @memberof Table
+   */
+  mapCell = (rowData, col, i) => {
     const { mapCells: mapCellsFunc, name, colClasses } = this.props;
     const colName = col.name;
     const cellAttrs = {
@@ -248,7 +422,7 @@ export default class Table extends Component {
       title: typeof rowData[colName] !== 'object' ? rowData[colName] : undefined
     }
 
-    const mutation = typeof mapCellsFunc === 'function' && mapCellsFunc(name, col.name, rowData) || {};
+    const mutation = typeof mapCellsFunc === 'function' && mapCellsFunc(name, col.name, rowData, cellAttrs) || {};
     let formatOptions;
     if (col.formatOpts) {
       formatOptions = deepMerge({ ...col.formatOpts }, mutation);
@@ -272,39 +446,93 @@ export default class Table extends Component {
     );
   }
 
+  /**
+   * Renders the table content.
+   *
+   * @method content
+   * @param {Array} children - Optional children to be rendered in the table.
+   * @returns {React.Component} - The rendered table.
+   * @memberof Table
+   */
   content(children = this.props.children) {
-    const { data, columns, headerClasses, hover, striped } = this.props;
+    const { data, columns, headerClasses, tableClasses, hover, striped, vertical } = this.props;
+
+    // Definir encabezado y pie de página si están presentes
     let header, footer;
     if (Array.isArray(children))
-      [header, footer] = children;
+      ([header, footer] = children);
+
+    // Construcción de las clases CSS para la tabla
     const cn = ['table'];
     if (striped) cn.push('table-striped');
     if (hover) cn.push('table-hover');
-    return <table className={cn.join(' ')}>
-      <thead>
-        {header &&
-          <tr>
-            <td colSpan="1000">
-              <div>{header}</div>
-            </td>
-          </tr>
+    if (tableClasses) cn.push(tableClasses);
+    if (vertical) cn.push('vertical');
+
+    // Inicialización de los datos de la tabla según la disposición (vertical/horizontal)
+    const tableData = [];
+
+    // Procesamiento de los datos para construir la estructura de la tabla
+    data.forEach((row, i) => {
+      Object.entries(columns).forEach(([key, col], j) => {
+        const column = col.name ? col : { name: key, ...col };
+
+        if (!vertical) {
+          // Si la tabla no es vertical (es decir, es horizontal)
+
+          // Si no se ha inicializado el registro de la tabla (fila) en la posición i, se inicializa
+          if (!tableData[i]) tableData[i] = { cells: [], data: row };
+
+          // Se mapea la celda en la posición j de la fila i, pasándole los datos de la fila y la columna
+          tableData[i].cells[j] = this.mapCell(row, column, j);
+        } else {
+          // Si la tabla es vertical
+
+          // Si no se ha inicializado el registro de la tabla (columna) en la posición j, se inicializa
+          if (!tableData[j]) tableData[j] = { cells: [], data: column };
+
+          // Para la primera fila de cada columna (i === 0), se mapea la celda de encabezado
+          if (i === 0) tableData[j].cells[0] = this.mapHeaderCell([key, column], 0);
+
+          // Para las filas restantes de cada columna, se mapea la celda correspondiente
+          const k = i + 1;
+          tableData[j].cells[k] = this.mapCell(row, column, k);
         }
-        <tr className={headerClasses}>
-          {Object.entries(columns).map(this.mapHeaderCell)}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(this.mapRows)}
-      </tbody>
-      {footer &&
-        <tfooter>
-          <tr>
-            <td colSpan="1000">
-              <div>{footer}</div>
-            </td>
-          </tr>
-        </tfooter>
-      }
-    </table>
+
+      });
+    });
+
+    // Renderización de la tabla con las estructuras de encabezado, cuerpo y pie de página
+    return (
+      <table className={cn.join(' ')}>
+        <thead>
+          {header && (
+            <tr>
+              <td colSpan="1000">
+                <div>{header}</div>
+              </td>
+            </tr>
+          )}
+          {!vertical && (
+            <tr className={headerClasses}>
+              {Object.entries(columns).map(this.mapHeaderCell)}
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          {tableData.map(({ cells, data }, i) => <tr {...this.rowProps(data, i)}>{cells}</tr>)}
+        </tbody>
+        {footer && (
+          <tfoot>
+            <tr>
+              <td colSpan="1000">
+                <div>{footer}</div>
+              </td>
+            </tr>
+          </tfoot>
+        )}
+      </table>
+    );
   }
+
 }
