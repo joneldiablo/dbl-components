04a24cfd5201625c708fdd9020d47d53a9215fbd
Jonathan Diego Rodriguez Rdz <jon_eldiablo@hotmail.com>
2023-05-31 16:45:51 -0600

    missing break

--- src/js/forms/fields/file-field.jsx
+++ src/js/forms/fields/file-field.jsx
@@ -65,6 +65,7 @@ export default class FileField extends Field {
           break;
         case 'zip':
           reader.readAsArrayBuffer(file);
+          break;
         default:
           break;
       }
