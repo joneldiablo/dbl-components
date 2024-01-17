aca3bea9f638c2403ee2a411f182a305d7b42660
joneldiablo <jon_eldiablo@hotmail.com>
2024-01-17 16:27:59 -0600

    feat(build): conditional version update and commit
    Add conditionals to the build script to check for a supplied message argument before updating the version and committing changes. This prevents unwanted updates and commits when no message is given. Additionally, npm publish now requires an OTP (One-Time Password), enhancing security during publication. Commented out the push to the backup remote to avoid redundancy or unintended pushes.

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
-node update-version.js
+if [ -n "$1" ]; then
+  node update-version.js
+fi
 rm -rf ./lib/js/* && mkdir -p lib/js
 rm -rf ./lib/css/* && mkdir -p lib/css
 rm -rf ./lib/scss/* && mkdir -p lib/scss
 cp -r src/scss lib
-# construir desde "arriba" para no instalar las dependencias aquí
-# hay pedos si se está trabajando con la librería linkada (yarn link)
+# Construir desde "arriba" para no instalar las dependencias aquí
+# Hay problemas si se está trabajando con la librería linkada (yarn link)
 cd ..
 yarn babel
 cd ./dbl-components
 #-------
-git add .
-git commit -m "$1"
-git push origin --all
-git push backup --all
-npm publish
+if [ -n "$1" ]; then
+  git add .
+  git commit -m "$1"
+  git push origin --all
+  #git push backup --all
+fi
+npm publish --otp
