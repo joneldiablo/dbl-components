cb1434af9214b3fce41ac6561ad44e64c1617aba
jonathandr <jonathan.rodriguez@cpavs.mx>
2024-07-12 18:44:33 -0600

    ADDED JsonRenderComponent for dynamic JSON rendering functionality
    This commit introduces a new component named `JsonRenderComponent`. This component is designed to perform dynamic rendering based on a provided JSON structure. Key functionalities include setting up props, default properties, component lifecycle methods (such as `componentDidMount` and `componentWillUnmount`), and a method to handle mutations and content building. This enhancement aims to improve the flexibility and scalability of the JSON rendering process in the application.
    
    🟢 src/js/json-render-component.js -> Implementation of the dynamic JSON rendering component

---
+++ src/js/json-render-component.js
@@ -0,0 +1,82 @@
+import PropTypes from "prop-types";
+
+import eventHandler from "./functions/event-handler";
+import resolveRefs from "./functions/resolve-refs";
+import deepMerge from "./functions/deep-merge";
+import JsonRender from "./json-render";
+import Component from "./component";
+
+export default class JsonRenderComponent extends Component {
+
+  static jsClass = 'JsonRenderComponent';
+  static template = {
+    view: {}, definitions: {}
+  };
+
+  static propTypes = {
+    ...Component.propTypes,
+    view: PropTypes.object,
+    childrenIn: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+    definitions: PropTypes.object
+  }
+
+  static defaultProps = {
+    ...Component.defaultProps,
+    view: null,
+    childrenIn: false,
+    definitions: {}
+  }
+
+  events = [];
+
+  constructor(props) {
+    super(props);
+    Object.assign(this.state, {});
+    this.jsonRender = new JsonRender(this.fixedProps, this.mutations.bind(this));
+    this.jsonRender.childrenIn = this.childrenIn;
+  }
+
+  get fixedProps() {
+    return this.props;
+  }
+
+  get childrenIn() {
+    return false;
+  }
+
+  get theView() {
+    return this.constructor.template.view;
+  }
+
+  componentDidMount() {
+    this.events.forEach(([evtName, callback]) => eventHandler.subscribe(evtName, callback, this.name));
+    const definitions = deepMerge(this.constructor.template.definitions || {}, this.props.definitions);
+
+    this.templateSolved = this.props.view
+      ? resolveRefs(this.props.view, {
+        template: this.theView,
+        definitions,
+        props: this.props,
+        state: this.state
+      })
+      : resolveRefs(this.theView, {
+        definitions,
+        props: this.props,
+        state: this.state
+      });
+
+  }
+
+  componentWillUnmount() {
+    this.events.forEach(([eName]) => eventHandler.unsubscribe(eName, this.name));
+  }
+
+  mutations(sectionName, section) {
+    return this.state[sectionName];
+  }
+
+  content() {
+    return this.jsonRender.buildContent(this.templateSolved);
+  }
+
+}
