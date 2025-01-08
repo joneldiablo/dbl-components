d6cca4afba0211a6e9e3bfbf3e58ad8332a924c7
jonathandr <jonathan.rodriguez@cpavs.mx>
2025-01-08 15:31:44 -0600

    ENHANCED: Added autoplay to HeroContainer and multi-header support to Table
    This commit adds autoplay functionality to the HeroContainer component by integrating Swiper's Autoplay module. It also allows additional Swiper properties to be merged via the `swiperpros` prop.
    
    In the Table component, support for multiple header rows has been added through the new `headerRows` prop. The handling of class names has been improved by utilizing the `splitAndFlat` utility function from 'dbl-utils'. PropTypes and defaultProps have been updated accordingly.
    
    🛠️ src/js/containers/hero-container.js -> Added autoplay functionality using Swiper's Autoplay module and support for additional Swiper properties via `swiperpros` prop
    
    🛠️ src/js/tables/table.js -> Added support for multiple header rows with `headerRows` prop and improved class name handling with `splitAndFlat` utility function

--- src/js/containers/hero-container.js
+++ src/js/containers/hero-container.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+import { deepMerge } from "dbl-utils";
 
 import Container from "./container";
 
 export default class HeroContainer extends Container {
 
   static jsClass = 'HeroContainer';
+
   static defaultProps = {
     ...Container.defaultProps,
     fullWidth: true,
@@ -24,19 +28,26 @@ export default class HeroContainer extends Container {
 
   content(children = this.props.children) {
     if (!this.breakpoint) return this.waitBreakpoint;
-    const { spaceBetween, slidesPerView } = this.props;
+    const { spaceBetween, slidesPerView, autoplayDelay, swiperpros } = this.props;
     let propsSwiper = {
       spaceBetween,
       slidesPerView,
+      modules: [Autoplay],
+      autoplay: {
+        delay: autoplayDelay || 6000,
+        disableOnInteraction: false,
+      },
       onSlideChange: this.onSlideChange,
       onSwiper: this.onSwiper,
-
     }
     if (children.length < 2) {
       Object.assign(propsSwiper, {
         resistance: true,
         resistanceRatio: 0
       });
+    }
+    if (typeof swiperpros === 'object') {
+      deepMerge(propsSwiper, swiperpros);
     }
     return (React.createElement(Swiper, { ...propsSwiper },
       children.map((slide, i) => {
