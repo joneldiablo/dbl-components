4ea1526ee108dce5c22fbec7827fcbd7c37f947b
Jonathan D Rodriguez <jon_eldiablo@hotmail.com>
2025-02-26 14:42:25 -0600

    🔧 chore(script): mejorar script de copia de archivos SCSS
    - cambiar shebang a sh para mayor compatibilidad
    - agregar verificación de archivos SCSS antes de la ejecución
    - mejorar el proceso de copia para crear directorios si es necesario
    
    🔧 chore(package): modificar comando cpInlineStyle en package.json
    
    - cambiar la ejecución del script de copia a usar la ruta relativa
    
    ♻️ refactor(containers): actualizar SlideContainer para usar Container
    
    - reemplazar la herencia de Component por Container
    - agregar propiedad perPage por defecto en las opciones del slider
    - mejorar la lógica de renderizado con verificación de breakpoint

--- cp-scss.sh
+++ cp-scss.sh
@@ -1,3 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-find src/js -type f -name "*.scss" -exec sh -c 'file="{}"; cp "$file" "lib/js/${file#src/js/}"' \;
+echo "copy scss..."
+
+# Verificar si hay archivos .scss en src/js antes de ejecutar find
+if ! find src/js -type f -name "*.scss" | grep -q .; then
+  echo "No SCSS files found in src/js, skipping copy."
+  exit 0
+fi
+
+# Si hay archivos, ejecuta el proceso de copia
+find src/js -type f -name "*.scss" | while IFS= read -r file; do
+  dest=$(echo "$file" | sed 's|^src/js/|lib/js/|')
+  mkdir -p "$(dirname "$dest")"
+  cp "$file" "$dest"
+done
