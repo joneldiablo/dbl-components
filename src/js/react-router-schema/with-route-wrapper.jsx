1df695d55c4c244cd4ea4a4b2bf86ae0fd5a5ab9
jonathan diego rdz <jon_eldiablo@hotmail.com>
2025-08-05 12:08:28 -0600

    fix wrapper to update location without unmount

--- src/js/react-router-schema/with-route-wrapper.jsx
+++ src/js/react-router-schema/with-route-wrapper.jsx
@@ -13,43 +13,54 @@ const withRouteWrapper = (WrappedComponent, route) => {
     const location = useLocation();
     const navigate = useNavigate();
     const params = useParams();
-
+    const [, forceUpdate] = React.useReducer((x) => x + 1, 0);
 
     useEffect(() => {
       // Manage body class based on route name
-      const viewClassName = Array.from(document.body.classList).find(cl => cl.endsWith('-view'));
+      const viewClassName = Array.from(document.body.classList).find((cl) =>
+        cl.endsWith("-view")
+      );
       if (viewClassName) {
         document.body.classList.remove(viewClassName);
       }
       document.body.classList.add(`${route.name}-view`);
 
       // Remove old location-based classes and add new location class
-      document.body.classList.forEach(cls => {
-        if (cls.startsWith('location-')) {
+      document.body.classList.forEach((cls) => {
+        if (cls.startsWith("location-")) {
           document.body.classList.remove(cls);
         }
       });
-      document.body.classList.add(`location${location.pathname.replace(/\//g, '-')}`);
+      document.body.classList.add(
+        `location${location.pathname.replace(/\//g, "-")}`
+      );
+
+      // Forzar actualización del componente si cambia la ruta
+      forceUpdate();
 
       // Apply custom styles to route if provided
       if (!route.style) {
         route.style = {};
       }
-      route.style['--component-name'] = `"${route.name}"`;
+      route.style["--component-name"] = `"${route.name}"`;
 
       // Cleanup function to remove added classes when the component unmounts or route changes
       return () => {
         document.body.classList.remove(`${route.name}-view`);
-        document.body.classList.remove(`location${location.pathname.replace(/\//g, '-')}`);
+        document.body.classList.remove(
+          `location${location.pathname.replace(/\//g, "-")}`
+        );
       };
-    }, [location.pathname, route.name]);
+    }, [location.pathname]);
 
     return (
-      <WrappedComponent {...props}
+      <WrappedComponent
+        {...props}
         location={location}
         navigate={navigate}
         match={params}
-        route={route} />
+        route={route}
+      />
     );
   };
 };
